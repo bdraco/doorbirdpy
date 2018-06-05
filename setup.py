b5464adb5b5a6765b6291d4533f68f6ef52b7263
Andy Castille <andy@robiotic.net>
2018-06-05 09:04:09 -0500

    move to gitea

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     author_email="andy@robiotic.net",
     packages=["doorbirdpy"],
     install_requires="httplib2",
-    url="https://github.com/Klikini/doorbirdpy",
-    download_url="https://github.com/Klikini/doorbirdpy/archive/master.zip",
+    url="https://git.robiotic.net/klikini/doorbirdpy",
+    download_url="https://git.robiotic.net/klikini/doorbirdpy/archive/master.zip",
     license="MIT",
     description="Python wrapper for the DoorBird LAN API v0.21",
     platforms="Cross Platform"

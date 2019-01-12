7c15f757f7cf4d84878e72623e424063c1b3a857
Andy Castille <andy@robiotic.net>
2019-01-12 21:33:31 +0000

    Increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="DoorBirdPy",
-    version="2.0.5",
+    version="2.0.6",
     author="Andy Castille",
     author_email="andy@robiotic.net",
     packages=["doorbirdpy"],

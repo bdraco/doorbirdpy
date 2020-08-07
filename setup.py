d787048b2e051ae2f5f1f0ca14c1ab2f363b1bcb
Andy Castille <me@andycc.dev>
2020-08-07 04:01:17 +0000

    Bump version, fix CI, and other project maintenance

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="DoorBirdPy",
-    version="2.0.8",
+    version="2.1.0",
     author="Andy Castille",
     author_email="andy@robiotic.net",
     packages=["doorbirdpy"],

c700d85c21a31b6f2d29a0a84a1bd8d2d579c637
J. Nick Koston <nick@koston.org>
2024-07-10 04:44:58 +0000

    Convert to asyncio

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ from setuptools import setup
 
 setup(
     name="DoorBirdPy",
-    version="2.2.2",
+    version="3.0.0",
     author="Andy Castille",
     author_email="andy@robiotic.net",
+    maintainer="J. Nick Koston",
+    maintainer_email="nick@koston.org",
     packages=["doorbirdpy"],
-    install_requires=["requests"],
+    install_requires=["aiohttp"],
     url="https://gitlab.com/klikini/doorbirdpy",
     download_url="https://gitlab.com/klikini/doorbirdpy/-/archive/master/doorbirdpy-master.zip",
     license="MIT",
+    python_requires=">=3.9",
     description="Python wrapper for the DoorBird LAN API v0.21",
     platforms="Cross Platform",
 )
